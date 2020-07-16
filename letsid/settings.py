2ea0c547d76b4096f66ddbab4d0740053a323836
devfdL <33900154+devfdL@users.noreply.github.com>
2020-07-17 01:20:46 +0700

    s

--- letsid/settings.py
+++ letsid/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5f@6&q)1%nl-n0wfq%bki2g!2_k%rkewcai60^c!$yq@6_#e=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'letsid.urls'
